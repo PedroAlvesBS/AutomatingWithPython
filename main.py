0589c16aba5a8db351b0ff7a0d7317e380369cda
iamwho <PEdro@pedro.com>
2022-02-15 22:13:09 -0300

    Final state

--- main.py
+++ main.py
@@ -27,3 +27,39 @@ cnt = extract_news('https://news.ycombinator.com/')
 content += cnt
 content += ('<br>--------<br>')
 content += ('<br><br>End of Message')
+
+#Sending the email
+print('Composing Email...')
+
+SERVER = 'smtp.gmail.com'
+PORT = 587
+FROM = ''
+TO = ''
+PASS = ''
+
+#fp = open(file_name, 'rb')
+#Create a text/plain message
+#msg = MIMEText('')
+msg = MIMEMultipart()
+
+#msg.add_header('Content-Disposition', 'attachment', filename='empty.txt')
+msg['SUbject'] = 'Top News Stories HN [Automated EMail]' + ' ' + str(now.day) + '-' + str(now.year)
+msg['From'] = FROM
+msg['To'] = TO
+
+msg.attach(MIMEText(content, 'html'))
+#fp.close
+
+print('Initiating Server...')
+server = smtplib.SMTP(SERVER, PORT)
+#server = smtplib.SMTP SSL('smtp.gmail.com', 465)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+#server.ehlo
+server.login(FROM, PASS)
+server.sendmail(FROM, TO, msg.as_string())
+
+print('Email Sent...')
+
+server.quit()
