20fd9bad2f049ef8373cdaf8a303e8ab71d5136a
PedroAlves <pepedro2512@gmail.com>
2022-02-17 13:32:17 -0300

    Formatting only

--- MailAutomation.py
+++ MailAutomation.py
@@ -9,8 +9,9 @@ from email.mime.text import MIMEText
 import datetime
 now = datetime.datetime.now()
 
-#Creating content
+# Creating content
 content = ''
+
 
 def extract_news(url):
     print('Extracting Hacker News Stories...')
@@ -19,17 +20,19 @@ def extract_news(url):
     response = requests.get(url)
     content = response.content
     soup = BeautifulSoup(content, 'html.parser')
-    for i, tag in enumerate(soup.find_all('td', attrs={'class':'title','valign':''})):
-        cnt += ((str(i+1)+' :: '+tag.text+'\n'+'<br>') if tag.text!='More' else '')
-        #print(ta,prettify)
+    for i, tag in enumerate(soup.find_all('td', attrs={'class': 'title', 'valign': ''})):
+        cnt += ((str(i+1)+' :: '+tag.text+'\n'+'<br>')
+                if tag.text != 'More' else '')
+        # print(ta,prettify)
     return(cnt)
+
 
 cnt = extract_news('https://news.ycombinator.com/')
 content += cnt
 content += ('<br>--------<br>')
 content += ('<br><br>End of Message')
 
-#Sending the email
+# Sending the email
 print('Composing Email...')
 
 SERVER = 'smtp.gmail.com'
@@ -39,25 +42,26 @@ TO = ''
 PASS = ''
 
 #fp = open(file_name, 'rb')
-#Create a text/plain message
+# Create a text/plain message
 #msg = MIMEText('')
 msg = MIMEMultipart()
 
 #msg.add_header('Content-Disposition', 'attachment', filename='empty.txt')
-msg['Subject'] = 'Top News Stories HN [Automated Email]' + ' ' + str(now.day) + '-' + str(now.year)
+msg['Subject'] = 'Top News Stories HN [Automated Email]' + \
+    ' ' + str(now.day) + '-' + str(now.year)
 msg['From'] = FROM
 msg['To'] = TO
 
 msg.attach(MIMEText(content, 'html'))
-#fp.close
+# fp.close
 
 print('Initiating Server...')
 server = smtplib.SMTP(SERVER, PORT)
-#server = smtplib.SMTP SSL('smtp.gmail.com', 465)
+# server = smtplib.SMTP SSL('smtp.gmail.com', 465)
 server.set_debuglevel(1)
 server.ehlo()
 server.starttls()
-#server.ehlo
+# server.ehlo
 server.login(FROM, PASS)
 server.sendmail(FROM, TO, msg.as_string())
 
